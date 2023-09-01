c7b1fed7718a008e5418bfe0b397927659e99e00
fadliasyhariStar <fadli.asyhari@starcapital.co.id>
2023-09-01 11:24:41 +0700

    fix git

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import unittest
 from fastapi import FastAPI
 import os
 import subprocess
@@ -7,15 +8,22 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    # Get Git Commit
-    git_commit = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().strip()
-
-    # Get Git Tag
-    git_tag = subprocess.check_output(["git", "tag", "-l", "--sort=-creatordate", "--format=%(refname:short)", "v*"]).decode().strip().split("\n")[0]
-
-    # Get Git Build Date
-    git_build_date = subprocess.check_output(["git", "log", "-1", "--format=%ct"]).decode().strip()
-    git_build_date_formatted = datetime.utcfromtimestamp(int(git_build_date)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    # run git commands to get info
+    try:
+        git_commit = subprocess.check_output(["git", "rev-parse", "HEAD"]).decode().strip()
+    except subprocess.CalledProcessError:
+        git_commit = ""
+    
+    try:
+        git_tag = subprocess.check_output(["git", "tag", "-l", "--sort=-creatordate", "--format=%(refname:short)", "v*"]).decode().strip().split("\n")[0]
+    except subprocess.CalledProcessError:
+        git_tag = ""
+    
+    try:
+        git_build_date = subprocess.check_output(["git", "log", "-1", "--format=%ct"]).decode().strip()
+        git_build_date = datetime.utcfromtimestamp(int(git_build_date)).strftime('%Y-%m-%dT%H:%M:%S.%dZ')
+    except (subprocess.CalledProcessError, ValueError):
+        git_build_date = ""
 
     # Set environment variables
     os.environ["GIT_COMMIT"] = git_commit
@@ -28,3 +36,10 @@ async def read_root():
         "git_tag": git_tag,
         "git_build_date": git_build_date_formatted,
     }
+
+if __name__ == "__main__":
+    print("\n*** EXECUTING UNIT TEST ***")
+    unittest.main(module='test_module', verbosity=2, exit=False)
+    print("*** UNIT TEST EXECUTED ***\n")
+    
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8080, reload=True)#, log_level="critical")
